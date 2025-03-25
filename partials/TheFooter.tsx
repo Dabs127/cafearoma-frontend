104356d645ee0bcefffea612dac467c2de662b08
Daniel <danielSerna127@outlook.com>
2025-03-25 11:47:03 -0400

    feat: Create carousel component and add it to the index

--- partials/TheFooter.tsx
+++ partials/TheFooter.tsx
@@ -4,14 +4,13 @@ import { FaFacebook, FaInstagram } from "react-icons/fa";
 export default function TheFooter() {
 
     return (
-        <footer className="w-full h-auto bg-primary p-5 flex flex-col justify-center items-center mt-auto">
-            <div className="flex gap-5">
+        <footer className="w-full h-auto bg-primary p-15 flex flex-col justify-center items-center mt-auto">
+            <div className="flex gap-5 my-5">
                 <FaFacebook className="text-secondary text-3xl" />
                 <FaInstagram className="text-secondary text-3xl" />
             </div>
-            <p className="text-secondary text-center">Café Aroma &copy; 2025</p>
+            <p className="text-secondary text-center mb-5">Café Aroma &copy; 2025</p>
             <Link href="#" className="text-secondary underline">Aviso de privacidad</Link>
-
         </footer>
     )
 }
