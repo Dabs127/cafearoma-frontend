cb2bd6beaf0e1201c1f270845aba8031024d7466
Daniel <danielSerna127@outlook.com>
2025-08-01 11:30:58 -0600

    fix: build errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import eslintPluginTypeScript from "@typescript-eslint/eslint-plugin";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -12,6 +13,9 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
+    plugins: {
+      "@typescript-eslint": eslintPluginTypeScript,
+    },
     rules: {
       "@typescript-eslint/ban-types": [
         "warn",
